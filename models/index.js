6ff2cad780b5726f7427c72e0e4f157c51175330
Joseph Knoebel <knoebelja@gmail.com>
2017-08-11 15:00:45 -0400

    Upto 2. Models

--- models/index.js
+++ models/index.js
@@ -1,4 +1,74 @@
 const Sequelize = require('sequelize');
 const db = new Sequelize('postgres://localhost:5432');
 
+
+// Place: address, city, state, phone, and location
+const Place = db.define('place', {
+  address: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  city: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  state: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  phone: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  location: {
+    type: Sequelize.ARRAY(Sequelize.FLOAT),
+    allowNull: false
+  }
+});
+
+// Hotel: name, num_stars. amenities
+const Hotel = db.define('hotel', {
+  name: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  num_stars: {
+    type: Sequelize.FLOAT,
+    allowNull: false
+  },
+  amenities: {
+    type: Sequelize.STRING,
+    allowNull: false
+  }
+});
+
+// Activity: name, age_range
+const Activity = db.define('activity', {
+  name: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  age_range: {
+    type: Sequelize.STRING,
+    allowNull: false
+  }
+});
+
+// Restaurant: name, cuisine, price
+const Restaurant = db.define('restaurant', {
+  name: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  cuisine: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  price: {
+    type: Sequelize.INTEGER,
+    allowNull: false
+  }
+});
+
+
 module.exports = db;
